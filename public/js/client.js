0d598932efd8b93df19350491c24c1278774b9e9
Joan Fluvià Poyatos <joanfluvia@gmail.com>
2021-04-17 15:31:05 +0200

    feat: Load texts (#6)
    * feat: load texts
    
    * feat: load texts

--- public/js/client.js
+++ public/js/client.js
@@ -1,75 +1,102 @@
-import ClientSocketManager from './clientSocketManager.js';
-import * as Utils from './utils.js';
+import ClientSocketManager from "./clientSocketManager.js";
+import * as Utils from "./utils.js";
 
 $(function () {
   let clientSocketManager = new ClientSocketManager();
 
   // Restore last nick
-  $("#nick").val(localStorage.getItem('lastNick'));
-  
+  $("#nick").val(localStorage.getItem("lastNick"));
+
   // Disable cache
-  $.ajaxSetup ({
-    cache: false
+  $.ajaxSetup({
+    cache: false,
   });
 
   // Keep a variable to know if the window has focus
-  $(window).focus(function() {
-    clientSocketManager.setWindowFocus(true);
-    document.title = "XatBy2";
-  }).blur(function() {
-    clientSocketManager.setWindowFocus(false);
-  });
-  
+  $(window)
+    .focus(function () {
+      clientSocketManager.setWindowFocus(true);
+      document.title = "XatBy2";
+    })
+    .blur(function () {
+      clientSocketManager.setWindowFocus(false);
+    });
+
   // Submit the login
-  $('body').on('submit', '#login', function() {
-    clientSocketManager.login($('#nick').val());
+  $("body").on("submit", "#login", function () {
+    clientSocketManager.login($("#nick").val());
     return false;
   });
-  
+
   // Submit a chat message
-  $('body').on('submit','#xat', function() {
-  	let nickColor = $('#nick-color').val();
-    clientSocketManager.sendMessage(nickColor, $('#message').val());
-    
+  $("body").on("submit", "#xat", function () {
+    let message = $("#message").val().trim();
+    let nickColor = $("#nick-color").val();
+    if (message === "") return;
+
+    clientSocketManager.sendMessage(nickColor, message);
+
     // We want the nick color stored across sessions
-    localStorage.setItem('nickColor', nickColor);
-    $('#message').val('');
+    localStorage.setItem("nickColor", nickColor);
+    $("#message").val("");
     return false;
   });
-  
+
   // On Windows resize
-  $(window).resize(function() {
+  $(window).resize(function () {
     Utils.updateChatListHeigth();
   });
-  
+
   //On chat icon image click (Open popup)
-  $("#xat-grid").on("click", "li img.chat-image", function() {
+  $("#xat-grid").on("click", "li img.chat-image", function () {
     $("#chat-image img").attr("src", $(this).data("src"));
     $("#chat-image").show();
   });
-  
+
   // On popup image click (Close popup)
-  $("#chat-image").click(function() {
+  $("#chat-image").click(function () {
     $("#chat-image").hide();
   });
-  
+
+  // On load File
+  $("#file-input").change(function () {
+    let file = $("#file-input")[0].files[0];
+    var reader = new FileReader();
+    reader.addEventListener("load", (event) => {
+      let text = event.target.result;
+      let texts = text
+        .split("\n")
+        .map((text) => text.trim())
+        .filter((text) => text.length > 0);
+      localStorage.setItem("texts", JSON.stringify(texts));
+      Utils.addMessage(
+        '<strong style="color:darkblue">' +
+          texts.length +
+          " Texts Loaded!</strong>",
+        true
+      );
+    });
+    reader.readAsText(file);
+  });
+
   // On paste from clipboard (an image)
-  document.onpaste = function(event) {
-  	let items = (event.clipboardData || event.originalEvent.clipboardData).items;
+  document.onpaste = function (event) {
+    let items = (event.clipboardData || event.originalEvent.clipboardData)
+      .items;
     for (let index in items) {
-    	let item = items[index];
-      if (item.kind === 'file') {
-      	let blob = item.getAsFile();
-      	let reader = new FileReader();
-        reader.onload = function(event) {
-        	let data = event.target.result;
-        	if (data.startsWith("data:image")) {
-        		let nickColor = $('#nick-color').val();
+      let item = items[index];
+      if (item.kind === "file") {
+        let blob = item.getAsFile();
+        let reader = new FileReader();
+        reader.onload = function (event) {
+          let data = event.target.result;
+          if (data.startsWith("data:image")) {
+            let nickColor = $("#nick-color").val();
             clientSocketManager.sendImage(nickColor, data);
-        	}
-        }
+          }
+        };
         reader.readAsDataURL(blob);
       }
     }
-  }
+  };
 });
--- public/js/clientSocketManager.js
+++ public/js/clientSocketManager.js
@@ -1,4 +1,4 @@
-import * as Utils from './utils.js';
+import * as Utils from "./utils.js";
 
 export default class ClientSocketManager {
   constructor() {
@@ -6,79 +6,160 @@ export default class ClientSocketManager {
     this._windowFocus = true;
     this._nick;
     this._users = {};
-    
+
     this._iniSocket();
   }
-  
+
   _iniSocket() {
     /* ACCEPTED MESSAGES */
-    this._socket.on('connect', () => this._connect());
-    this._socket.on('disconnect', () => this._disconnect());
-    this._socket.on('chat message', msg => this._chatMessage(msg));
-    this._socket.on('chat image', msg => this._chatImage(msg));
-    this._socket.on('command message', msg => this._commandMessage(msg));
-    this._socket.on('login message', msg => this._loginMessage(msg));
-    this._socket.on('disconnect message', msg => this._disconnectMessage(msg));
-    this._socket.on('need login', () => this._needLogin());
-    this._socket.on('login error', msg => this._loginError(msg));
-    this._socket.on('login success', () => this._loginSuccess());
+    this._socket.on("connect", () => this._connect());
+    this._socket.on("disconnect", () => this._disconnect());
+    this._socket.on("chat message", (msg) => this._chatMessage(msg));
+    this._socket.on("chat image", (msg) => this._chatImage(msg));
+    this._socket.on("command message", (msg) => this._commandMessage(msg));
+    this._socket.on("login message", (msg) => this._loginMessage(msg));
+    this._socket.on("disconnect message", (msg) =>
+      this._disconnectMessage(msg)
+    );
+    this._socket.on("need login", () => this._needLogin());
+    this._socket.on("login error", (msg) => this._loginError(msg));
+    this._socket.on("login success", () => this._loginSuccess());
   }
-  
+
   /* SOCKET FUNCTIONS */
   // CONNECT
   _connect() {
-    if ($('#xat-grid').length) {
-      Utils.translate("message.connect", message => Utils.addSystemMessage(message, this._windowFocus));
+    if ($("#xat-grid").length) {
+      Utils.translate("message.connect", (message) =>
+        Utils.addSystemMessage(message, this._windowFocus)
+      );
       // Cache the disconnect message to be able to retrieve it when the server goes down
       Utils.translate("message.disconnect");
     }
   }
-  
+
   // DISCONNECT
   _disconnect() {
-    if ($('#xat-grid').length) {
-      Utils.translate("message.disconnect", message => Utils.addSystemMessage(message, this._windowFocus));
+    if ($("#xat-grid").length) {
+      Utils.translate("message.disconnect", (message) =>
+        Utils.addSystemMessage(message, this._windowFocus)
+      );
     }
   }
-  
+
   // CHAT MESSAGE
-  _chatMessage({message, nick, nickColor}) {
+  _chatMessage({ message, nick, nickColor }) {
     // Escape text from message to avoid html injection:
-    let escapedMessage = $('<p></p>').text(message).html();
-    let escapedNickColor = $('<p></p>').text(nickColor).html();
-    let escapedNick = $('<p></p>').text(nick).html();
-    Utils.addMessage('<strong style="color:' + escapedNickColor + '">' + escapedNick + ':</strong> ' + escapedMessage, this._windowFocus);
+    let escapedMessage = $("<p></p>").text(message).html();
+    let escapedNickColor = $("<p></p>").text(nickColor).html();
+    let escapedNick = $("<p></p>").text(nick).html();
+    Utils.addMessage(
+      '<strong style="color:' +
+        escapedNickColor +
+        '">' +
+        escapedNick +
+        ":</strong> " +
+        escapedMessage,
+      this._windowFocus
+    );
   }
-  
+
   // CHAT IMAGE
-  _chatImage({image, nick, nickColor}) {
+  _chatImage({ image, nick, nickColor }) {
     // Escape text from message to avoid html injection:
-    let escapedImage = $('<p></p>').text(image).html();
-    let escapedNickColor = $('<p></p>').text(nickColor).html();
-    let escapedNick = $('<p></p>').text(nick).html();
-    Utils.addMessage('<strong style="color:' + escapedNickColor + '">' + escapedNick +
-        ':</strong> <img class="chat-image" src="/img/popupImage.png" data-src=" ' + escapedImage + '" />', this._windowFocus);
+    let escapedImage = $("<p></p>").text(image).html();
+    let escapedNickColor = $("<p></p>").text(nickColor).html();
+    let escapedNick = $("<p></p>").text(nick).html();
+    Utils.addMessage(
+      '<strong style="color:' +
+        escapedNickColor +
+        '">' +
+        escapedNick +
+        ':</strong> <img class="chat-image" src="/img/popupImage.png" data-src=" ' +
+        escapedImage +
+        '" />',
+      this._windowFocus
+    );
   }
-  
+
+  // LOCAL COMMAND
+  _localParse(nickColor, command) {
+    let localCommands = {
+      loadTexts: () => this._loadTexts(),
+      delTexts: () => this._delTexts(),
+      text: (params) => this._printText(nickColor, params),
+      t: (params) => this._printText(nickColor, params),
+    };
+    let isLocalCommand = false;
+
+    let tokens = command.trim().split(" ");
+    let [instruction, ...params] = tokens;
+
+    if (localCommands[instruction]) {
+      isLocalCommand = true;
+      localCommands[instruction](params);
+    }
+    return isLocalCommand;
+  }
+
+  _loadTexts() {
+    $("#file-input").trigger("click");
+  }
+
+  _delTexts() {
+    localStorage.removeItem("texts");
+    Utils.addMessage(
+      '<strong style="color:darkblue">Texts Deleted</strong>',
+      true
+    );
+  }
+
+  _printText(nickColor, params) {
+    let texts = JSON.parse(localStorage.getItem("texts"));
+    let index = -1;
+    let param = params[0];
+
+    if (texts.length === 0) return;
+    index = parseInt(param);
+
+    if (isNaN(index) || index < 0 || index > texts.length) {
+      index = Math.floor(Math.random() * texts.length);
+    }
+
+    let message = texts[index];
+    this._socket.emit("chat message", { nickColor, message });
+  }
+
   // COMMAND MESSAGE
   _commandMessage(message) {
-    Utils.addMessage('<strong style="color:darkred">' + message + '</strong>', this._windowFocus);
+    Utils.addMessage(
+      '<strong style="color:darkred">' + message + "</strong>",
+      this._windowFocus
+    );
   }
-  
+
   // LOGIN MESSAGE
-  _loginMessage({nick, users}) {
-    Utils.translate("message.user.connected", message => Utils.addSystemMessage(message, this._windowFocus), {"user":nick});
+  _loginMessage({ nick, users }) {
+    Utils.translate(
+      "message.user.connected",
+      (message) => Utils.addSystemMessage(message, this._windowFocus),
+      { user: nick }
+    );
     this._users = users;
     this._updateUsers();
   }
-    
+
   // DISCONNECT MESSAGE
-  _disconnectMessage({nick, users}) {
-    Utils.translate("message.user.disconnected", message => Utils.addSystemMessage(message, this._windowFocus), {"user":nick});
+  _disconnectMessage({ nick, users }) {
+    Utils.translate(
+      "message.user.disconnected",
+      (message) => Utils.addSystemMessage(message, this._windowFocus),
+      { user: nick }
+    );
     this._users = users;
     this._updateUsers();
   }
-  
+
   // NEED LOGIN
   _needLogin() {
     // If we have the nick cached send it, else show the login window
@@ -91,50 +172,53 @@ export default class ClientSocketManager {
   }
   // LOGIN ERROR
   _loginError(message) {
-    $('#login-error').html(message);
+    $("#login-error").html(message);
   }
-  
+
   // LOGIN SUCCESS
   _loginSuccess() {
-    let nickColor = localStorage.getItem('nickColor');
-    localStorage.setItem('lastNick', this._nick);
-    
+    let nickColor = localStorage.getItem("nickColor");
+    localStorage.setItem("lastNick", this._nick);
+
     $("#login").hide();
     $("#xat-grid").show();
     this._updateUsers();
     Utils.updateChatListHeigth();
     $("#message").focus();
-    
+
     // Restore nick color
     if (nickColor) {
       $("#nick-color").val(nickColor);
     }
   }
-  
+
   /* AUXILIAR FUNCTIONS */
   _updateUsers() {
     Utils.updateUsers(this._users, this._nick);
   }
-  
+
   /* PUBLIC FUNTIONS */
   login(nick) {
     this._nick = nick;
-    this._socket.emit('login', this._nick);
+    this._socket.emit("login", this._nick);
   }
-  
+
   sendMessage(nickColor, message) {
-    this._socket.emit('chat message', {nickColor, message});
+    if (message.startsWith("/")) {
+      if (this._localParse(nickColor, message.substring(1))) return;
+    }
+    this._socket.emit("chat message", { nickColor, message });
   }
-  
+
   sendImage(nickColor, image) {
-    this._socket.emit('chat image', {nickColor, image});
+    this._socket.emit("chat image", { nickColor, image });
   }
-  
+
   /* GETTERS AND SETTERS */
   isWindowFocus() {
     return this._windowFocus;
   }
-  
+
   setWindowFocus(windowFocus) {
     this._windowFocus = windowFocus;
   }
