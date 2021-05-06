dea8a879b544699fd49cf92dfb61e86043c6371e
Joan Fluvià Poyatos <joanfluvia@gmail.com>
2021-05-06 08:56:33 +0200

    feat: upgrade socket.io and express version

--- server.js
+++ server.js
@@ -1,21 +1,23 @@
 const PORT = process.env.PORT || 3000;
 
-let express = require('express'),
-  app = express(),
-  http = require('http').Server(app),
-  io = require('socket.io')(http),
-  cookieParser = require('cookie-parser'),
-  bodyParser = require('body-parser');
-  i18n = require("i18n"),
-  ServerSocketManager = require('./serverSocketManager'),
-  ServerCommandParser = require('./serverCommandParser'),
-  Utils = require('./utils');
-  
+let express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+(i18n = require("i18n")),
+  (ServerSocketManager = require("./serverSocketManager")),
+  (ServerCommandParser = require("./serverCommandParser")),
+  (Utils = require("./utils"));
+
 // set the view engine to ejs
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 // set the public folder
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // use cookieParser to expose cookies to req.cookies
 app.use(cookieParser());
@@ -28,10 +30,10 @@ app.use(bodyParser.json());
 
 // set the i18n configuration
 i18n.configure({
-	defaultLocale: 'en',
-  cookie: 'i18n',
-  directory: __dirname + '/locales',
-  updateFiles: false
+  defaultLocale: "en",
+  cookie: "i18n",
+  directory: __dirname + "/locales",
+  updateFiles: false,
 });
 
 console.log("Locales detected: " + i18n.getLocales());
@@ -40,38 +42,44 @@ console.log("Locales detected: " + i18n.getLocales());
 app.use(i18n.init);
 
 // one page app
-app.get('/', function(req, res) {
+app.get("/", (req, res) => {
   if (req.cookies.i18n) {
     res.setLocale(req.cookies.i18n);
   } else {
-    res.cookie('i18n','en');
-    res.setLocale('en');
+    res.cookie("i18n", "en");
+    res.setLocale("en");
   }
-  res.render('main', {'i18n': res});
+  res.render("main", { i18n: res });
 });
 
 // change default language
-app.get('/:lang(en|es|ca)/', function (req, res) {
-  res.cookie('i18n', req.params.lang);
-  res.redirect('/');
+app.get("/:lang(en|es|ca)/", (req, res) => {
+  res.cookie("i18n", req.params.lang);
+  res.redirect("/");
 });
 
 // get translation (req.body.key / req.body.params(JSON.stringified))
-app.post('/translate', function (req, res) {
-  res.json(res.__(req.body.key, JSON.parse(req.body.params ? req.body.params : "{}")));
+app.post("/translate", (req, res) => {
+  res.json(
+    res.__(req.body.key, JSON.parse(req.body.params ? req.body.params : "{}"))
+  );
 });
-
 
 let users = {};
 
-io.on('connection', function(socket){
+io.on("connection", (socket) => {
   // set the lang(i18n) in the request
   i18n.init(socket.request);
   let i18nCookie = Utils.getCookie("i18n", socket.handshake.headers.cookie);
-  socket.request.setLocale(i18nCookie ? i18nCookie : 'en');
-    
+  socket.request.setLocale(i18nCookie ? i18nCookie : "en");
+
   let serverCommandParser = new ServerCommandParser(socket, io);
-  let serverSocketManager = new ServerSocketManager(socket, io, users, serverCommandParser);
+  let serverSocketManager = new ServerSocketManager(
+    socket,
+    io,
+    users,
+    serverCommandParser
+  );
 });
 
-http.listen(PORT, () => console.log(`Listening on ${ PORT }`));
+server.listen(PORT, () => console.log(`Listening on ${PORT}`));
--- serverSocketManager.js
+++ serverSocketManager.js
@@ -6,80 +6,108 @@ module.exports = class ServerSocketManager {
     this._users = users;
     this._serverCommandParser = serverCommandParser;
     this._req = socket.request;
-    
+
     this._iniSocket();
   }
-  
+
   /* INITIALIZE SOCKET */
   _iniSocket() {
     console.log("New socket: " + this._socket.id);
-    this._socket.emit('need login');
-    
+    this._socket.emit("need login");
+
     /* ACCEPTED MESSAGES */
-    this._socket.on('login', msg => this._login(msg));
-    this._socket.on('disconnect', () => this._disconnect());
-    this._socket.on('chat message', msg => this._chatMessage(msg));
-    this._socket.on('chat image', msg => this._chatImage(msg));
+    this._socket.on("login", (msg) => this._login(msg));
+    this._socket.on("disconnect", () => this._disconnect());
+    this._socket.on("chat message", (msg) => this._chatMessage(msg));
+    this._socket.on("chat image", (msg) => this._chatImage(msg));
   }
-  
+
   /* SOCKET FUNCTIONS */
   // LOGIN
   _login(nick) {
-  	let nickRegex = /^[a-zA-Z\-_]{4,15}$/;
-  	if (typeof nick !== "string" || !nick.match(nickRegex)) {
-  		this._socket.emit('login error', this._req.__('message.username.restriction'));
-  	} else if (!this._users[nick]) {
+    let nickRegex = /^[a-zA-Z\-_]{4,15}$/;
+    if (typeof nick !== "string" || !nick.match(nickRegex)) {
+      this._socket.emit(
+        "login error",
+        this._req.__("message.username.restriction")
+      );
+    } else if (!this._users[nick]) {
       this._nick = nick;
       this._serverCommandParser.setNick(nick);
       this._users[nick] = this._socket;
-      
+
       //Connecting to the general room:
-      this._socket.join('general');
-      
-      this._socket.emit('login success');
-      this._io.to('general').emit('login message', {'nick':this._nick, 'users':Object.keys(this._users).sort()}); //TODO pass message
+      this._socket.join("general");
+
+      this._socket.emit("login success");
+      this._io.to("general").emit("login message", {
+        nick: this._nick,
+        users: Object.keys(this._users).sort(),
+      }); //TODO pass message
 
       console.log("User connected: " + this._nick + " :: " + this._socket.id);
     } else {
-      this._socket.emit('login error', this._req.__('message.username.in.use', {user: nick}));
+      this._socket.emit(
+        "login error",
+        this._req.__("message.username.in.use", { user: nick })
+      );
     }
   }
-  
+
   // DISCONNECT
   _disconnect() {
-  	console.log("User disconnected: " + this._nick + " :: " + this._socket.id);
-  	
+    console.log("User disconnected: " + this._nick + " :: " + this._socket.id);
+
     if (this._nick) {
       delete this._users[this._nick];
-      this._io.to('general').emit('disconnect message', {'nick':this._nick, 'users':Object.keys(this._users).sort()}); //TODO pass message
+      this._io.to("general").emit("disconnect message", {
+        nick: this._nick,
+        users: Object.keys(this._users).sort(),
+      }); //TODO pass message
     }
   }
-  
+
   // CHAT MESSAGE
-  _chatMessage({message, nickColor}) {
+  _chatMessage({ message, nickColor }) {
     //Check the user is currently in the general room (logged)
-    if (this._socket.rooms.general && typeof message === "string" && typeof nickColor === "string") {
+    if (
+      this._socket.rooms.has("general") &&
+      typeof message === "string" &&
+      typeof nickColor === "string"
+    ) {
       if (message.startsWith("/")) {
-      	this._socket.emit('chat message', {message, nickColor, nick: this._nick});
+        this._socket.emit("chat message", {
+          message,
+          nickColor,
+          nick: this._nick,
+        });
         this._serverCommandParser.parse(message.substring(1));
       } else {
-        this._io.to('general').emit('chat message', {message, nickColor, nick: this._nick});
+        this._io
+          .to("general")
+          .emit("chat message", { message, nickColor, nick: this._nick });
       }
     }
   }
-  
+
   // CHAT IMAGE
-  _chatImage({image, nickColor}) {
+  _chatImage({ image, nickColor }) {
     //Check the user is currently in the general room (logged)
-    if (this._socket.rooms.general && typeof image === "string" && typeof nickColor === "string") {
-      this._io.to('general').emit('chat image', {image, nickColor, nick: this._nick});
+    if (
+      this._socket.rooms.has("general") &&
+      typeof image === "string" &&
+      typeof nickColor === "string"
+    ) {
+      this._io
+        .to("general")
+        .emit("chat image", { image, nickColor, nick: this._nick });
     }
   }
-  
+
   /* AUXILIAR FUNCTIONS */
   _getCookie(name, cookies) {
     var value = "; " + cookies;
     var parts = value.split("; " + name + "=");
     if (parts.length == 2) return parts.pop().split(";").shift();
   }
-}
+};
